e2f4a776125d2c25aba6c637167127ea302805e7
Matthew Sienkiewich <matthew.sienkiewich@wsu.edu>
2024-06-14 19:30:37 -0700

    Daily Question setup

--- DailyProblemTemplate.py
+++ DailyProblemTemplate.py
@@ -1,10 +1,29 @@
 """
-A simple starter python file
+LeetCode Daily Question 502
+IPO
 
-Goal:   
+Goal:   Suppose LeetCode will start its IPO soon. In order to sell a good price of its shares to Venture Capital,
+        LeetCode would like to work on some projects to increase its capital before the IPO. Since it has limited resources,
+        it can only finish at most k distinct projects before the IPO. Help LeetCode design the best way to maximize its total 
+        capital after finishing at most k distinct projects.
+
+        You are given n projects where the ith project has a pure profit profits[i] and a minimum capital of capital[i] is needed to start it.
+        Initially, you have w capital. When you finish a project, you will obtain its pure profit and the profit will be added to your total capital.
+        Pick a list of at most k distinct projects from given projects to maximize your final capital, and return the final maximized capital.
 
 Matthew Sienkiewich
 """
 
 import sys
 import os
+
+def findMaximizedCapital(self, k, w, profits, capital):
+    """
+    :type k: int
+    :type w: int
+    :type profits: List[int]
+    :type capital: List[int]
+    :rtype: int
+    """
+
+    
